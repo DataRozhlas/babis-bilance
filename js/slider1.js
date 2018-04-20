eb40d42d0096d5bb0efa7e7075003d055bba1560
Petr Kočí <petr.koci@samizdat.cz>
2018-04-20 23:41:22 +0200

    druhý a třetí karusel

--- js/slider1.js
+++ js/slider1.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.slider').slick({
+  $('.slider1').slick({
   slidesToShow: 3,
   slidesToScroll: 3,
   autoplay: true,
@@ -7,7 +7,7 @@ $(document).ready(function(){
   dots: true,
   accessibility: false,
   pauseOnDotsHover: true,
-  autoplaySpeed: 4000,
+  autoplaySpeed: 3500,
   responsive: [
     {
       breakpoint: 600,
@@ -20,9 +20,43 @@ $(document).ready(function(){
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots: false
       }
     }
   ]
+});
+  $('.slider2').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  autoplay: true,
+  infinite: true,
+  dots: true,
+  accessibility: false,
+  pauseOnDotsHover: true,
+  autoplaySpeed: 4500,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
+    }]
 });
 });
